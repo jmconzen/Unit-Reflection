1f05c0e8929b89d9a43822fc1cc3b4c70a569f04
jmconzen <149109238+jmconzen@users.noreply.github.com>
2023-10-26 22:49:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const beforeUnitQuestions = [
-  "Why do we think that the unit or the selection of topics will be interesting?",
+  "Why do we think that this unit or the selection of topics will be interesting?",
   "What do students already know, and what can they do?",
   "What have students encountered in this discipline before?",
   "What does experience tell us about what to expect in this unit?",
   "What attributes of the learner profile does this unit offer students opportunities to develop?",
   "What potential interdisciplinary connections can we identify for this unit?",
   "What do we know about students’ preferences and patterns of interaction?",
-  "Are there any possible opportunities for meaningful service learning?",
+  "Are there any possible opportunities for meaningful service learning in this unit?",
   "What in the unit might be inspiring for community or personal projects?",
-  "Could we develop authentic opportunities for service learning?",
   "How can we use students’ multilingualism as a resource for learning?"
 ];
 
@@ -17,7 +16,7 @@ const duringUnitQuestions = [
   "What resources are proving useful, and what other resources do we need?",
   "What student inquiries are emerging?",
   "What can we adjust or change?",
-  "What skills need more practice?",
+  "What skills do students need more practice with?",
   "What is the level of student engagement?",
   "How can we scaffold learning for students who need more guidance?",
   "What is happening in the world right now with which we could connect teaching and learning in this unit?",
@@ -42,16 +41,20 @@ const afterUnitQuestions = [
   "What did we learn from standardizing the assessment?"
 ];
 
+const unitTypeElement = document.getElementById('unitType');
 const questionElement = document.getElementById('question');
-const beforeUnitBtn = document.getElementById('beforeUnitBtn');
-const duringUnitBtn = document.getElementById('duringUnitBtn');
-const afterUnitBtn = document.getElementById('afterUnitBtn');
 
 function getRandomQuestion(questions) {
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  questionElement.textContent = questions[randomIndex];
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    return questions[randomIndex];
 }
 
-beforeUnitBtn.addEventListener('click', () => getRandomQuestion(beforeUnitQuestions));
-duringUnitBtn.addEventListener('click', () => getRandomQuestion(duringUnitQuestions));
-afterUnitBtn.addEventListener('click', () => getRandomQuestion(afterUnitQuestions));
+function generateQuestion(questions, unitType) {
+    const generatedQuestion = getRandomQuestion(questions);
+    unitTypeElement.textContent = unitType;
+    questionElement.textContent = generatedQuestion;
+}
+
+document.getElementById('beforeUnitBtn').addEventListener('click', () => generateQuestion(beforeUnitQuestions, "Before Unit:"));
+document.getElementById('duringUnitBtn').addEventListener('click', () => generateQuestion(duringUnitQuestions, "During Unit:"));
+document.getElementById('afterUnitBtn').addEventListener('click', () => generateQuestion(afterUnitQuestions, "After Unit:"));
